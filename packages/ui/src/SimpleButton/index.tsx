6519bfc05b5a03fc39a2df4366a3c7b8659fd768
sungjinkim1986 <sungjin.kim@medithings.co.kr>
2025-11-05 09:47:29 +0900

    add css-in-js from ui module.

--- packages/ui/src/SimpleButton/index.tsx
+++ packages/ui/src/SimpleButton/index.tsx
@@ -2,13 +2,18 @@
 
 import { useEffect } from 'react';
 import { execute } from 'core-util';
+import * as S from './styles';
 
 const SimpleButton = () => {
     useEffect(() => {
         execute();
     }, []);
 
-    return <div>simple button</div>;
+    return (
+        <S.Container>
+            <div>simple button</div>
+        </S.Container>
+    );
 }
 
 export default SimpleButton;

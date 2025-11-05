04701f7213a8981abec44ecf0a2075397370a29b
sungjinkim1986 <sungjin.kim@medithings.co.kr>
2025-11-05 09:32:35 +0900

    convert admin project.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import { SimpleButton } from 'core-ui';
+
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <SimpleButton />
         <Image
           className={styles.logo}
           src="/next.svg"
